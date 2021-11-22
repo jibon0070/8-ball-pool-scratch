a4720031cba0f110a2c6dbbf28c169bccdb7afa4
A.R. Jibon <atikurrahaman386@gmail.com>
2021-11-22 20:11:43 +0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {APP_BASE_HREF, PlatformLocation} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ export class AppComponent implements OnInit {
   private size!: number;
   private container!: HTMLDivElement;
 
-  constructor(readonly _title: Title) {
+  constructor(readonly _title: Title, readonly platformLocation: PlatformLocation) {
   }
 
   readonly possible_values = ["250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300", "250k", "100k", "500", "100", "5k", "1.5k", "50k", "10k", "25", "50", "300"];
@@ -32,14 +33,13 @@ export class AppComponent implements OnInit {
     this.container.style.height = `${this.size}px`;
     for (let i = 0; i < 9; i++) {
       const cell = document.createElement('div');
-      cell.style.background = "url('/assets/scratch cover.png')";
+      cell.style.background = `url('${this.platformLocation.getBaseHrefFromDOM()}assets/scratch cover.png')`;
       cell.style.backgroundSize = "cover"
       cell.style.width = `${this.size / 3}px`
       cell.style.height = `${this.size / 3}px`
       cell.style.border = "1px solid black"
       cell.style.overflow = "hidden"
       const cell_value = this.possible_values[Math.floor(Math.random() * (this.possible_values.length - 1))]
-      console.log(cell_value)
       this.container.append(cell);
       let clicked = false;
       cell.addEventListener('mousedown', ev => {
@@ -60,7 +60,7 @@ export class AppComponent implements OnInit {
           bubble.style.position = "absolute";
           bubble.style.width = "4%";
           bubble.style.height = "4%";
-          bubble.style.background = `url('/assets/${cell_value}.png')`;
+          bubble.style.background = `url('${this.platformLocation.getBaseHrefFromDOM()}assets/${cell_value}.png')`;
           bubble.style.backgroundSize = `${this.size / 3}px`;
           bubble.style.backgroundPositionX = `${cell.getBoundingClientRect().left}px`;
           bubble.style.backgroundPositionY = `${cell.getBoundingClientRect().top}px`;
@@ -70,6 +70,25 @@ export class AppComponent implements OnInit {
           bubble.style.borderRadius = "50%";
           cell.appendChild(bubble);
         }
+      });
+      cell.addEventListener("touchmove", ev => {
+        ev.preventDefault()
+        const bubble = document.createElement('span');
+        bubble.style.left = `${ev.touches[0].clientX - ((this.size / 3)) * 4 / 100}px`
+        bubble.style.top = `${ev.touches[0].clientY - ((this.size / 3)) * 4 / 100}px`
+        bubble.style.display = "block";
+        bubble.style.position = "absolute";
+        bubble.style.width = "4%";
+        bubble.style.height = "4%";
+        bubble.style.background = `url('${this.platformLocation.getBaseHrefFromDOM()}assets/${cell_value}.png')`;
+        bubble.style.backgroundSize = `${this.size / 3}px`;
+        bubble.style.backgroundPositionX = `${cell.getBoundingClientRect().left}px`;
+        bubble.style.backgroundPositionY = `${cell.getBoundingClientRect().top}px`;
+        bubble.style.backgroundRepeat = `no-repeat`;
+        bubble.style.backgroundAttachment = "fixed";
+        bubble.style.pointerEvents = "none";
+        bubble.style.borderRadius = "50%";
+        cell.appendChild(bubble);
       });
     }
   }
